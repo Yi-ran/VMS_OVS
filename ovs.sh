2abbd7491a60b0698a013a5fbc29e47aaf4d7926
Yiran Zhang <yiran914@gmail.com>
2018-11-07 18:31:51 +0800

    Update ovs.sh

--- ovs.sh
+++ ovs.sh
@@ -37,7 +37,7 @@ if [ $? -eq 0 ] ; then
    # ovs-vsctl add-br br0
    # ovs-vsctl add-port br0 enp2s0f1
       ifconfig eno2 up
-      ip addr add 192.168.1.10/24 dev br0
+      #ip addr add 192.168.1.10/24 dev br0
       #ip addr add 192.168.1.11/24 dev br1
       ip link set br0 up
       #ip link set br1 up
@@ -46,7 +46,7 @@ if [ $? -eq 0 ] ; then
       #arp -s 192.168.3.1 38:97:d6:f5:39:15
    #ip link set enp2s0f0 down
    #ip link set enp2s0f1 down
-      ip route add 192.168.3.11 via 192.168.1.1 dev br1
+      #ip route add 192.168.3.11 via 192.168.1.1 dev br1
      # ip route add 192.168.3.10 via 192.168.1.1 dev br0
      # ethtool -K br0 tso off
      # ethtool -K br0 gro off
