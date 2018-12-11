bc00c642dca02adfd185497b52a55510e2735761
Yiran Zhang <yiran914@gmail.com>
2018-12-11 15:49:52 +0800

    Update parse-tcp-rtt.py

--- parse-tcp-rtt.py
+++ parse-tcp-rtt.py
@@ -17,28 +17,27 @@ path = sys.argv[1]
 
 for bench in os.listdir(path):
   sub_path = path + "/" + bench.strip()
-  log = open("./" + bench.strip() + "-rtt-10g", "w")
+  #log = open("./" + bench.strip() + "-rtt-10g", "w")
   print "Entering" + sub_path
 
-  for t in os.listdir(sub_path):
-    sub_sub_path = sub_path + "/" + t
-    log.write("====" + t + "\n")
-    print "Entering" + sub_sub_path
+  #for t in os.listdir(sub_path):
+    #sub_sub_path = sub_path + "/" + t
+    #log.write("====" + t + "\n")
+    #print "Entering" + sub_sub_path
 
-    for f in os.listdir(sub_sub_path):
-      print "Processing" + sub_sub_path + "/" + f
-      proc = subprocess.Popen(cmd%(sub_sub_path + "/" + f), \
-          shell=True, stdout=subprocess.PIPE)
-      acc = 0
-      counter = 0
-      for line in iter(proc.stdout.readline, ""):
-        rtt = float(line.strip())
-        if rtt > 0:
-          acc += rtt
-          counter += 1
-      if counter > 0:
-        result = f + " " + str(acc / counter * 1000) # millisecond
-      print result
-      log.write(result + "\n")
+    #for f in os.listdir(sub_sub_path):
+      #print "Processing" + sub_sub_path + "/" + f
+  proc = subprocess.Popen(cmd%(sub_path), shell=True, stdout=subprocess.PIPE)
+  acc = 0
+  counter = 0
+  for line in iter(proc.stdout.readline, ""):
+    rtt = float(line.strip())
+    if rtt > 0:
+      acc += rtt
+      counter += 1
+  if counter > 0:
+    result = " " + str(acc / counter * 1000) # millisecond
+  print result
+  #log.write(result + "\n")
 
-  log.close()
+  #log.close()
