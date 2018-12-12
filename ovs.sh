23062a5f2ff2e0639a01a6db96485dd0f6b35134
Yiran Zhang <yiran914@gmail.com>
2018-12-12 22:42:01 +0800

    Update ovs.sh

--- ovs.sh
+++ ovs.sh
@@ -7,9 +7,9 @@ ovs-dpctl del-dp ovs-system
 rmmod openvswitch
 
 echo installing....
-cp datapath_ovs_2.8.c ../openvswitch-2.8.0/datapath/linux/datapath.c
-cd ../openvswitch-2.8.0
-make #-j12 >/dev/null 2>/dev/null
+cp VMS_OVS/datapath_ovs_2.8.c openvswitch-2.8.0/datapath/linux/datapath.c
+cd openvswitch-2.8.0
+make  #-j12 >/dev/null 2>/dev/null
 
 if [ $? -eq 0 ] ; then
     make install >/dev/null
@@ -37,20 +37,18 @@ if [ $? -eq 0 ] ; then
    # ovs-vsctl add-port br1 enp3s0f1
    # ovs-vsctl add-br br0
    # ovs-vsctl add-port br0 enp2s0f1
-      #ifconfig eno2 up
-      #ip addr add 192.168.1.10/24 dev br0
+      ip addr add 192.168.3.9/24 dev br0
       #ip addr add 192.168.1.11/24 dev br1
       ip link set br0 up
       #ip link set br1 up
-      #arp -s 192.168.3.1 11:11:11:11:11:22
+      arp -s 192.168.3.1 00:00:00:00:66:fe
       #arp -s 192.168.4.1 11:11:11:11:11:23
       #arp -s 192.168.3.1 38:97:d6:f5:39:15
    #ip link set enp2s0f0 down
    #ip link set enp2s0f1 down
-      #ip route add 192.168.3.11 via 192.168.1.1 dev br1
+      ip route add 192.168.2.0/24 via 192.168.3.1
      # ip route add 192.168.3.10 via 192.168.1.1 dev br0
-     # ethtool -K br0 tso off
-     # ethtool -K br0 gro off
+      #ethtool -K br0 tx off
       #ethtool -K br1 tx off
       echo done
 else
